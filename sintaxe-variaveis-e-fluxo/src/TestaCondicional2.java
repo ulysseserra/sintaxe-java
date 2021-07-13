c7dc370d87127a81866383c571aa6b7c98fc4a0e
Ulysses Serra <72045801+ulyssesserra@users.noreply.github.com>
2021-07-13 16:20:20 -0400

    Adicionando arquivos via upload

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -0,0 +1,25 @@
+
+public class TestaCondicional2 {
+
+	public static void main(String[] args) {
+		System.out.println("Testando condicionais");
+		int idade = 20;
+		int quantidadePessoas = 3;
+		boolean acompanhado = quantidadePessoas >= 2;
+		
+		
+		System.out.println("Valor de acompanhado = " + acompanhado);
+		//if (idade >= 18 || quantidadePessoas >= 2)
+		if (idade >= 18 && acompanhado) {
+			System.out.println("Seja bem-vindo");
+		} else {
+			System.out.println("Infelizemente você não pode entrar");
+		}
+
+	}
+}
+
+
+
+//|| ou
+//&& ê
---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,24 @@
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		
+		//float pontoFlutuante = 3.14f;
+		
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		long numeroGrande = 4254785258l;
+		//short valorPequeno = 21315;
+		//byte b = 127;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		
+		System.out.println(total);
+		
+	}
+
+}
